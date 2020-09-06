7f503061f32091787c82f3d28669eb7af9107ffd
gmenin <gabrielgm@ufcspa.edu.br>
2020-09-06 02:45:04 -0300

    Inserção de comentários e update nome de componentes

--- Atividade2-Aula7-22_08_2020/src/atividade1/Prontuario.java
+++ Atividade2-Aula7-22_08_2020/src/atividade1/Prontuario.java
@@ -26,6 +26,7 @@ import javax.swing.JScrollPane;
 
 public class Prontuario extends JFrame {
 	
+	// Declaração global dos componentes
 	private JPanel contentPane;
 	private JTabbedPane tabbedPane;
 	private JTextField fieldNome;
@@ -64,12 +65,15 @@ public class Prontuario extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 650, 450);
 		
+		// Barra de menu
 		JMenuBar barraDeMenu = new JMenuBar();
 		setJMenuBar(barraDeMenu);
 		
+		// Menu
 		JMenu mnMenu = new JMenu("Menu");
 		barraDeMenu.add(mnMenu);
 		
+		// Item de menu => Sair
 		JMenuItem menuItemSair = new JMenuItem("Sair");
 		menuItemSair.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -77,94 +81,94 @@ public class Prontuario extends JFrame {
 			}
 		});
 		
-		mnMenu.add(menuItemSair);
+		mnMenu.add(menuItemSair); // inserção de item no menu
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		tabbedPane = new JTabbedPane(JTabbedPane.BOTTOM);
+		tabbedPane = new JTabbedPane(JTabbedPane.BOTTOM); // Container de painéis
 		contentPane.add(tabbedPane, BorderLayout.CENTER);
 		
-		JPanel painelDadosPessoais = new JPanel();
+		JPanel painelDadosPessoais = new JPanel(); // Primeiro painel (painel de dados)
 		painelDadosPessoais.setBackground(new Color(95, 158, 160));
-		tabbedPane.addTab("Dados Pessoais", new ImageIcon(Prontuario.class.getResource("/figuras/folder.png")), painelDadosPessoais, null);
+		tabbedPane.addTab("Dados Pessoais", new ImageIcon(Prontuario.class.getResource("/figuras/folder.png")), painelDadosPessoais, null); // Setando ícone na guia dados pessoais do tabbedPane
 		painelDadosPessoais.setLayout(new MigLayout("", "[][grow][grow][grow]", "[grow][grow][grow][grow][grow][grow]"));
 		
-		JLabel labelNome = new JLabel("Nome");
-		painelDadosPessoais.add(labelNome, "cell 0 0,alignx left");
+		JLabel labelNome = new JLabel("Nome"); 
+		painelDadosPessoais.add(labelNome, "cell 0 0,alignx left"); 
 		
-		fieldNome = new JTextField();
-		painelDadosPessoais.add(fieldNome, "cell 1 0 3 1,growx");
+		fieldNome = new JTextField(); // Campo de nome do paciente
+		painelDadosPessoais.add(fieldNome, "cell 1 0 3 1,growx"); // Inserção do campo no painel
 		fieldNome.setColumns(10);
 		
-		JLabel labelCPF = new JLabel("CPF");
-		painelDadosPessoais.add(labelCPF, "cell 0 1,alignx left");
+		JLabel labelCPF = new JLabel("CPF"); 
+		painelDadosPessoais.add(labelCPF, "cell 0 1,alignx left"); 
 		
-		textFieldCPF = new JTextField();
-		painelDadosPessoais.add(textFieldCPF, "cell 1 1,growx");
+		textFieldCPF = new JTextField(); // Campo de cpf do paciente
+		painelDadosPessoais.add(textFieldCPF, "cell 1 1,growx"); // Inserção do campo no painel
 		textFieldCPF.setColumns(10);
 		
 		JLabel labelNaturalidade = new JLabel("Naturalidade");
 		painelDadosPessoais.add(labelNaturalidade, "flowx,cell 2 1,alignx right");
 		
-		fieldNaturalidade = new JTextField();
-		painelDadosPessoais.add(fieldNaturalidade, "cell 3 1,growx");
+		fieldNaturalidade = new JTextField(); // Campo de cidade de origem do paciente
+		painelDadosPessoais.add(fieldNaturalidade, "cell 3 1,growx"); // Inserção do campo no painel
 		fieldNaturalidade.setColumns(10);
 		
 		JLabel labelSexo = new JLabel("Sexo");
 		painelDadosPessoais.add(labelSexo, "cell 0 2,alignx left");
 		
-		JComboBox comboBoxSexo = new JComboBox();
-		comboBoxSexo.setModel(new DefaultComboBoxModel(new String[] {"Masculino", "Feminino"}));
-		painelDadosPessoais.add(comboBoxSexo, "cell 1 2,growx");
+		JComboBox comboBoxSexo = new JComboBox(); // Caixa de combinação do sexo do paciente
+		comboBoxSexo.setModel(new DefaultComboBoxModel(new String[] {"Masculino", "Feminino"})); // Definição dos parâmetros da caixa de combinação
+		painelDadosPessoais.add(comboBoxSexo, "cell 1 2,growx"); // Inserção do componente no painel
 		
 		JLabel labelDataNascimento = new JLabel("Data de nascimento");
 		painelDadosPessoais.add(labelDataNascimento, "cell 2 2,alignx right");
 		
-		fieldDataNascimento = new JTextField();
-		painelDadosPessoais.add(fieldDataNascimento, "cell 3 2,growx");
+		fieldDataNascimento = new JTextField(); // Campo de data de nascimento do paciente
+		painelDadosPessoais.add(fieldDataNascimento, "cell 3 2,growx"); // Inserção do campo no painel
 		fieldDataNascimento.setColumns(10);
 		
-		JLabel labelEstadoCivil = new JLabel("Estado Civil");
+		JLabel labelEstadoCivil = new JLabel("Estado Civil"); 
 		painelDadosPessoais.add(labelEstadoCivil, "cell 0 3,alignx trailing");
 		
-		JComboBox comboBoxEstadoCivil = new JComboBox();
-		comboBoxEstadoCivil.setModel(new DefaultComboBoxModel(new String[] {"Solteiro", "Uniao Estavel", "Casado", "Divorciado", "Viuvo"}));
-		painelDadosPessoais.add(comboBoxEstadoCivil, "cell 1 3,growx");
+		JComboBox comboBoxEstadoCivil = new JComboBox(); // Caixa de combinação do estado civil do paciente
+		comboBoxEstadoCivil.setModel(new DefaultComboBoxModel(new String[] {"Solteiro", "Uniao Estavel", "Casado", "Divorciado", "Viuvo"})); // Definição dos parâmetros da caixa de combinação
+		painelDadosPessoais.add(comboBoxEstadoCivil, "cell 1 3,growx"); // Inserção do componente no painel
 		
 		JLabel labelTelefone = new JLabel("Telefone");
 		painelDadosPessoais.add(labelTelefone, "cell 2 3,alignx right");
 		
-		fieldTelefone = new JTextField();
-		painelDadosPessoais.add(fieldTelefone, "cell 3 3,growx");
+		fieldTelefone = new JTextField(); // Campo de telefone do paciente
+		painelDadosPessoais.add(fieldTelefone, "cell 3 3,growx"); // Inserção do componente no painel
 		fieldTelefone.setColumns(10);
 		
 		JLabel labelEndereco = new JLabel("Endere\u00E7o");
 		painelDadosPessoais.add(labelEndereco, "cell 0 4,alignx left");
 		
-		fieldEndereco = new JTextField();
-		painelDadosPessoais.add(fieldEndereco, "cell 1 4 3 1,growx");
+		fieldEndereco = new JTextField(); // Campo de telefone do paciente
+		painelDadosPessoais.add(fieldEndereco, "cell 1 4 3 1,growx"); // Inserção do componente no painel
 		fieldEndereco.setColumns(10);
 		
 		JButton buttonSalvarDadosPessoais = new JButton("Salvar");
-		buttonSalvarDadosPessoais.setIcon(new ImageIcon(Prontuario.class.getResource("/figuras/ok1.png")));
+		buttonSalvarDadosPessoais.setIcon(new ImageIcon(Prontuario.class.getResource("/figuras/ok1.png"))); // Setando ícone no botão
 		painelDadosPessoais.add(buttonSalvarDadosPessoais, "cell 0 5 4 1,alignx center");
 		
-		JPanel painelAnamnese = new JPanel();
+		JPanel painelAnamnese = new JPanel(); // Segundo painel (painel de anamnese)
 		painelAnamnese.setBackground(new Color(144, 238, 144));
-		tabbedPane.addTab("Anamnese", new ImageIcon(Prontuario.class.getResource("/figuras/conversa.png")), painelAnamnese, null);
+		tabbedPane.addTab("Anamnese", new ImageIcon(Prontuario.class.getResource("/figuras/conversa.png")), painelAnamnese, null); // Setando ícone na guia anamnese do tabbedPane
 		painelAnamnese.setLayout(new MigLayout("", "[grow][grow]", "[grow][grow][grow][grow][grow][grow][grow]"));
 		
 		JLabel labelQueixaPrincipal = new JLabel("Queixa Principal");
 		painelAnamnese.add(labelQueixaPrincipal, "cell 0 0");
 		
-		JScrollPane scrollPaneQueixaPrincipal = new JScrollPane();
+		JScrollPane scrollPaneQueixaPrincipal = new JScrollPane(); 
 		painelAnamnese.add(scrollPaneQueixaPrincipal, "cell 0 1 2 1,grow");
 		
-		JTextArea textAreaQP = new JTextArea();
-		scrollPaneQueixaPrincipal.setViewportView(textAreaQP);
+		JTextArea textAreaQueixaPrincipal = new JTextArea(); // Área de texto para queixa do paciente
+		scrollPaneQueixaPrincipal.setViewportView(textAreaQueixaPrincipal); 
 		
 		JLabel labelHistoricoDoenca = new JLabel("Hist\u00F3rico da Doen\u00E7a Atual");
 		painelAnamnese.add(labelHistoricoDoenca, "cell 0 2");
@@ -175,14 +179,14 @@ public class Prontuario extends JFrame {
 		JScrollPane scrollPaneHistoricoDoenca = new JScrollPane();
 		painelAnamnese.add(scrollPaneHistoricoDoenca, "cell 0 3,grow");
 		
-		JTextArea textAreaHDA = new JTextArea();
-		scrollPaneHistoricoDoenca.setViewportView(textAreaHDA);
+		JTextArea textAreaHistoricoDoenca = new JTextArea(); // Área de texto para histórico da doença no paciente
+		scrollPaneHistoricoDoenca.setViewportView(textAreaHistoricoDoenca);
 		
 		JScrollPane scrollPaneHistoricoFamiliar = new JScrollPane();
 		painelAnamnese.add(scrollPaneHistoricoFamiliar, "cell 1 3,grow");
 		
-		JTextArea textAreaHF = new JTextArea();
-		scrollPaneHistoricoFamiliar.setViewportView(textAreaHF);
+		JTextArea textAreaHistoricoFamiliar = new JTextArea(); // Área de texto do historico familiar do paciente
+		scrollPaneHistoricoFamiliar.setViewportView(textAreaHistoricoFamiliar);
 		
 		JLabel labelAntecedentes = new JLabel("Antecedentes pessoais");
 		painelAnamnese.add(labelAntecedentes, "cell 0 4");
@@ -193,35 +197,35 @@ public class Prontuario extends JFrame {
 		JScrollPane scrollPaneAntecedentes = new JScrollPane();
 		painelAnamnese.add(scrollPaneAntecedentes, "cell 0 5,grow");
 		
-		JTextArea textAreaAP = new JTextArea();
-		scrollPaneAntecedentes.setViewportView(textAreaAP);
+		JTextArea textAreaAntecedentes = new JTextArea(); // Área de texto para antecedentes do paciente
+		scrollPaneAntecedentes.setViewportView(textAreaAntecedentes);
 		
 		JScrollPane scrollPaneHabitos = new JScrollPane();
 		painelAnamnese.add(scrollPaneHabitos, "cell 1 5,grow");
 		
-		JTextArea textAreaHabitos = new JTextArea();
+		JTextArea textAreaHabitos = new JTextArea(); // Área de texto para hábitos do paciente
 		scrollPaneHabitos.setViewportView(textAreaHabitos);
 		
 		JButton buttonSalvarAnamnese = new JButton("Salvar");
-		buttonSalvarAnamnese.setIcon(new ImageIcon(Prontuario.class.getResource("/figuras/ok1.png")));
+		buttonSalvarAnamnese.setIcon(new ImageIcon(Prontuario.class.getResource("/figuras/ok1.png"))); // Setando ícone no botão
 		painelAnamnese.add(buttonSalvarAnamnese, "cell 0 6 2 1,alignx center");
 		
-		JPanel painelExames = new JPanel();
+		JPanel painelExames = new JPanel(); // Terceiro painel (painel de exames)
 		painelExames.setBackground(new Color(255, 165, 0));
-		tabbedPane.addTab("Exames", new ImageIcon(Prontuario.class.getResource("/figuras/test.png")), painelExames, null);
+		tabbedPane.addTab("Exames", new ImageIcon(Prontuario.class.getResource("/figuras/test.png")), painelExames, null); // Setando ícone na guia exames do tabbedPane
 		painelExames.setLayout(new MigLayout("", "[][grow]", "[grow][grow][grow][grow][grow]"));
 		
 		JLabel labelTipoExame = new JLabel("Tipo de Exame");
 		painelExames.add(labelTipoExame, "cell 0 0,alignx trailing");
 		
-		textFieldExame = new JTextField();
+		textFieldExame = new JTextField(); // Campo de texto para o nome do exame do paciente
 		painelExames.add(textFieldExame, "cell 1 0,growx");
 		textFieldExame.setColumns(10);
 		
-		JLabel labelData = new JLabel("Data");
+		JLabel labelData = new JLabel("Data"); 
 		painelExames.add(labelData, "cell 0 1,alignx left");
 		
-		textFieldData = new JTextField();
+		textFieldData = new JTextField(); // Campo de texto para data do exame do paciente
 		painelExames.add(textFieldData, "cell 1 1,alignx left");
 		textFieldData.setColumns(10);
 		
@@ -232,18 +236,18 @@ public class Prontuario extends JFrame {
 		painelExames.add(textAreaLaudo, "cell 1 2,growx");
 		
 		JButton buttonSalvarExames = new JButton("Salvar");
-		buttonSalvarExames.setIcon(new ImageIcon(Prontuario.class.getResource("/figuras/ok1.png")));
+		buttonSalvarExames.setIcon(new ImageIcon(Prontuario.class.getResource("/figuras/ok1.png"))); // Setando ícone no botão
 		painelExames.add(buttonSalvarExames, "cell 0 4 2 1,alignx center");
 		
-		JPanel painelTratamento = new JPanel();
+		JPanel painelTratamento = new JPanel(); // Quarto painel (painel de tratamento)
 		painelTratamento.setBackground(new Color(233, 150, 122));
-		tabbedPane.addTab("Tratamento", new ImageIcon(Prontuario.class.getResource("/figuras/remedio.png")), painelTratamento, null);
+		tabbedPane.addTab("Tratamento", new ImageIcon(Prontuario.class.getResource("/figuras/remedio.png")), painelTratamento, null); // Setando ícone na guia tratamento do tabbedPane
 		painelTratamento.setLayout(new MigLayout("", "[grow]", "[grow][grow][grow][grow][grow][grow]"));
 		
 		JLabel labelTratamentoTipo = new JLabel("Tipo de Tratamento");
 		painelTratamento.add(labelTratamentoTipo, "cell 0 0");
 		
-		textFieldTratamentoTipo = new JTextField();
+		textFieldTratamentoTipo = new JTextField(); // Campo de texto para o nome do tipo de tratamento do paciente
 		painelTratamento.add(textFieldTratamentoTipo, "cell 0 1,growx");
 		textFieldTratamentoTipo.setColumns(10);
 		
@@ -253,8 +257,8 @@ public class Prontuario extends JFrame {
 		TextArea textAreaTratamentoDescricao = new TextArea();
 		painelTratamento.add(textAreaTratamentoDescricao, "cell 0 3,growx");
 		
-		JButton buttonSalvarTratamento = new JButton("Salvar");
-		buttonSalvarTratamento.setIcon(new ImageIcon(Prontuario.class.getResource("/figuras/ok1.png")));
+		JButton buttonSalvarTratamento = new JButton("Salvar"); 
+		buttonSalvarTratamento.setIcon(new ImageIcon(Prontuario.class.getResource("/figuras/ok1.png"))); // Setando ícone no botão
 		painelTratamento.add(buttonSalvarTratamento, "cell 0 5,alignx center");
 	}
 
